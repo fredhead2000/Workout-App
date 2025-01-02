6f4fb8437d0a4ee9a5354032ad5ce9d395bd3d58
fredhead2000 <122188589+fredhead2000@users.noreply.github.com>
2025-01-02 06:36:07 -0500

    script.js

--- script.js
+++ script.js
@@ -1,25 +1,39 @@
 // Workout data
 const workouts = {
     Monday: [
-        { name: "Plank", image: "images/plank.jpg", sets: 3, reps: "30 seconds" },
-        { name: "Dead Bug", image: "images/deadbug.jpg", sets: 3, reps: "10 reps per side" },
-        { name: "Mountain Climbers", image: "images/mountain_climber.jpg", sets: 3, reps: "20 seconds" },
+        { name: "Forearm Plank to Roll-Outs", image: "images/plank_rollouts.jpg", sets: 3, reps: "10-12 reps" },
+        { name: "Dead Bugs", image: "images/deadbugs.jpg", sets: 3, reps: "10 reps per side" },
+        { name: "Seated Knee Lifts", image: "images/seated_knee_lifts.jpg", sets: 3, reps: "15 reps" },
+        { name: "Standing Dumbbell Side Bends", image: "images/side_bends.jpg", sets: 3, reps: "10 reps per side" },
+        { name: "Leg Raises", image: "images/leg_raises.jpg", sets: 3, reps: "12-15 reps" },
     ],
     Tuesday: [
-        { name: "Push Ups", image: "images/pushups.jpg", sets: 3, reps: "12 reps" },
-        { name: "Bodyweight Squats", image: "images/squats.jpg", sets: 3, reps: "15 reps" },
+        { name: "Incline Push-Ups", image: "images/incline_pushups.jpg", sets: 3, reps: "8-12 reps" },
+        { name: "Bodyweight Squats", image: "images/squats.jpg", sets: 3, reps: "12 reps" },
+        { name: "Forearm Plank Hold", image: "images/plank_hold.jpg", sets: 3, reps: "30 seconds" },
+        { name: "Rolling Ab Machine (Slow Control)", image: "images/rolling_ab.jpg", sets: 3, reps: "8 reps" },
+        { name: "Seated Russian Twists", image: "images/russian_twists.jpg", sets: 3, reps: "20 twists" },
     ],
     Wednesday: [
-        { name: "Side Plank", image: "images/side_plank.jpg", sets: 3, reps: "20 seconds per side" },
-        { name: "Knee Tucks", image: "images/knee_tucks.jpg", sets: 3, reps: "12 reps" },
+        { name: "Seated Knee Tucks", image: "images/knee_tucks.jpg", sets: 3, reps: "10-12 reps" },
+        { name: "Plank Shoulder Taps", image: "images/plank_taps.jpg", sets: 3, reps: "20 taps (10 per side)" },
+        { name: "Side Planks", image: "images/side_plank.jpg", sets: 3, reps: "20 seconds per side" },
+        { name: "Rolling Ab Machine Roll-Outs", image: "images/rolling_ab.jpg", sets: 3, reps: "8 reps" },
+        { name: "Dumbbell Side Raises", image: "images/side_raises.jpg", sets: 3, reps: "10 reps" },
     ],
     Thursday: [
+        { name: "Rolling Ab Machine Roll-Outs", image: "images/rolling_ab.jpg", sets: 3, reps: "10-12 reps" },
         { name: "Bird Dog", image: "images/bird_dog.jpg", sets: 3, reps: "10 reps per side" },
-        { name: "Reverse Lunges", image: "images/reverse_lunges.jpg", sets: 3, reps: "10 reps per leg" },
+        { name: "Bodyweight Reverse Lunges", image: "images/reverse_lunges.jpg", sets: 3, reps: "10 reps per leg" },
+        { name: "Forearm Plank with Leg Lifts", image: "images/plank_leg_lifts.jpg", sets: 3, reps: "20 seconds" },
+        { name: "Dumbbell Deadlifts", image: "images/deadlifts.jpg", sets: 3, reps: "12 reps" },
     ],
     Friday: [
-        { name: "Bicycle Crunches", image: "images/bicycle_crunches.jpg", sets: 3, reps: "15 reps per side" },
+        { name: "Rolling Ab Machine Roll-Outs", image: "images/rolling_ab.jpg", sets: 3, reps: "8-10 reps" },
+        { name: "Bicycle Crunches", image: "images/bicycle_crunches.jpg", sets: 3, reps: "10 reps per side" },
         { name: "Plank Walkouts", image: "images/plank_walkouts.jpg", sets: 3, reps: "8 reps" },
+        { name: "Side Plank Dips", image: "images/side_plank_dips.jpg", sets: 3, reps: "10 dips per side" },
+        { name: "Seated Dumbbell Presses", image: "images/dumbbell_presses.jpg", sets: 3, reps: "10 reps" },
     ],
 };
 
